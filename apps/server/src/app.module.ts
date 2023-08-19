5fc60f6f83c7ef438b99e245ee33b026a7eed2df
pyeonne <pyeonne@gmail.com>
2023-08-19 22:57:39 +0900

    🐛 FIX: Application failed to respond

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrpcModule } from './trpc/trpc.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TrpcModule],
+  imports: [ConfigModule.forRoot(), TrpcModule],
   controllers: [AppController],
   providers: [AppService],
 })
