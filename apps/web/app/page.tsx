9954075add9aaa0b6a56afd4a82b3aa93dde29c1
pyeonne <pyeonne@gmail.com>
2023-08-21 23:20:15 +0900

    📦 NEW: sort imports & prettier

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import { trpc } from "./trpc";
-import ClientSide from "./client_side";
+import ClientSide from './client_side'
+import { trpc } from './trpc'
 
 export default async function Home() {
-  const { greeting } = await trpc.hello.query({ name: `Tom` });
+  const { greeting } = await trpc.hello.query({ name: 'Tom' })
   return (
     <div>
       <p className="font-player text-[57px]">Server side = {greeting}</p>
       <ClientSide />
     </div>
-  );
+  )
 }
